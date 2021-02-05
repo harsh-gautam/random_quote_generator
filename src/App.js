0d27c4b5c3bbfe43ddc57a85bed2d85bb1c2e301
Harsh Gautam <hgautam208@gmail.com>
2021-02-05 19:55:47 +0530

    added new quote event handler

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import $ from 'jquery';
-import Popper from 'popper.js';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faFacebookSquare, faTwitterSquare } from '@fortawesome/free-brands-svg-icons'
+import {faQuoteLeft, faQuoteRight} from "@fortawesome/free-solid-svg-icons"
 
 import React from 'react';
 
@@ -14,30 +13,41 @@ class App extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            color: 'green',
+            color: '',
             quote: '',
             author: ''
         };
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    colors_arr = ['#ee0202', '#1c00db', '#61d800', '#f0c200', '#ef0078', '#00d2e1', '#ee7802', '#ee02e6', '#87ac00', '#7500d3   '];
+
+    componentDidMount() {
+        this.handleSubmit();
     }
 
     handleSubmit() {
-
+        fetch('https://api.quotable.io/random')
+            .then(response => response.json())
+            .then(data => {
+                this.setState({
+                    color: this.colors_arr[Math.floor(Math.random() * this.colors_arr.length)],
+                    quote: data.content,
+                    author: data.author
+                });
+            })
     }
 
     render() {
         return (
             <div className="container-fluid" style={{ backgroundColor: this.state.color }}>
                 <div id="quote-box" className="mx-auto card quote-card">
+                
                 <p id="text" style={{ color: this.state.color }}>
-                    loremalksddlas
-                    askldlasdasdla
-                    askdmslakdlskad
-                    askdlasdlkasdla
-                    askldmlaskdlak
-                    aklsdmlaksdlkasjdlksjdlka
-                    aslkdalsdlsadjalksjdlksajdlkasjdlkasjdwi
+                    <FontAwesomeIcon icon={faQuoteLeft} style={{paddingRight: 10, fontSize: 35}} />
+                    {this.state.quote}
                     </p>
-                <p id="author" style={{ color: this.state.color }}>-author</p>
+                <p id="author" style={{ color: this.state.color }}>- {this.state.author}</p>
                 <div className="d-flex buttons">
                     <div className="p-2 my-auto">
                         <a href="#" target="_blank" id="fb" style={{ color: this.state.color }}>
@@ -45,12 +55,12 @@ class App extends React.Component {
                         </a>
                     </div>
                     <div className="p-2 my-auto">
-                        <a href="#" target="_blank" id="tweet-quote" style={{ color: this.state.color }}>
+                        <a href="https://www.twitter.com/intent/tweet" target="_blank" id="tweet-quote" style={{ color: this.state.color }}>
                             <FontAwesomeIcon icon={faTwitterSquare} />
                         </a>
                     </div>
                     <div className="p-2 flex-grow-1 my-auto">
-                        <button id="new-quote" className="btn btn-info border-0" style={{ backgroundColor: this.state.color }}>
+                        <button id="new-quote" className="btn btn-info border-0" style={{ backgroundColor: this.state.color }} onClick={this.handleSubmit}>
                             New Quote
                         </button>
                     </div>
